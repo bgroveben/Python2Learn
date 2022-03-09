d4d0935e5a24481c6ffde88ceaf9da36923dd85b
bgroveben <bgroveben@gmail.com>
2022-03-09 12:44:32 -0800

    Add game functionality and make styling changes

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style>
+header, footer {
+  background-color: #3f7cad;
+}
+</style>
