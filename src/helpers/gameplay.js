08a1e9bd163c8433eae348d8080b70150159768f
bgroveben <bgroveben@gmail.com>
2022-05-22 12:00:26 -0700

    Remove unused components and add vue router

--- src/helpers/gameplay.js
+++ src/helpers/gameplay.js
@@ -1,32 +1,3 @@
-export function checkAnswer(input,correctAnswer) {
-  let fullAnswer = String(input);
-  if (parseInt(fullAnswer) !== correctAnswer) { 
-      return false;
-  } else { //answer is correct
-        return true;
-  }
-}
-  
-
-export function getRandNumbers(operator,low,high) {
-  let num1 = randInt(low, high);
-  let num2 = randInt(low, high, true);
-  let numHigh = Math.max(num1, num2);
-  let numLow = Math.min(num1, num2);
-  if(operator==='-') {
-    num1 = numHigh;
-    num2 = numLow;
-  }
-  if(operator==='/') {
-      while (num2 === 0) { // No division by zero
-        num2 = randInt(low, high);
-      }
-      num1 = (num1 * num2);
-    }
-  return({num1,num2});
-}
-  
-
 export function randInt(low, high, weighted=false) {
   let rndDec = Math.random();
   if (!weighted) {
@@ -42,21 +13,5 @@ export function randInt(low, high, weighted=false) {
   let rndInt = numList[Math.floor(Math.random() * numList.length)];
   return rndInt;
 }
-  
 
-export function getCorrectAnswer(oper,num1,num2) {
-  switch(oper) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case 'x':
-      return num1 * num2;
-    case '/':
-      return num1 / num2;
-    default:
-      return;
-  }
-}
-  
   
