2bd71a318db362391c4d78f060c698404debd097
bgroveben <bgroveben@gmail.com>
2022-09-30 11:09:13 -0700

    Get Math Facts Beta version working

--- math_facts.py
+++ math_facts.py
@@ -1,44 +1,71 @@
 import random
+import time
 from MathFacts import MathFacts
 from Calculator import Calculator
-#from Evaluator import Evaluator
 
 
-# from collections import namedtuple
-#Point = namedtuple('Point', 'x, y')
-# question = namedtuple('x', 'y', '+')
+operation = MathFacts.choose_op()
+maximum = MathFacts.set_max_num()
+game_length = 10
+start = time.time()
 
-def do_math():
-    operation = MathFacts.choose_op()
-    maximum = MathFacts.set_max_num()
+
+def run_game():
+    if time.time() - start > (game_length + 1):
+        print(round((time.time() - start),1))
+        print("Game Over")
+        return None
+    do_math(maximum, operation)
+
+
+def do_math(maximum, operation):
     x = random.randint(1, maximum)
     y = random.randint(1, maximum)
     equation = f"{x} {operation} {y} = ?: "
     answer = Calculator(x,y,operation).result
+    print(round((time.time() - start),1))
     print(equation)
     print(answer)
-    return answer
+    check_answer(answer)
+
+
+def is_number(n):
+    if n.isnumeric():
+        return n
+    else:
+        return False
+
+
+def validate_input():
+    user_answer = input("Enter an answer: ")
+    while not is_number(user_answer):
+        if time.time() - start > (game_length + 1):
+            print(round((time.time() - start),1))
+            print("Game Over")
+            return None
+        user_answer = input("Invalid Entry. Enter an answer: ")
+    return user_answer
 
 
 def check_answer(result):
+    user_answer = validate_input()
     try:
-        user_answer = float(input("Enter an answer: "))
-    except ValueError:
-        print("Please enter a number")
-
-    while user_answer != result:
-        print(f"{user_answer} is not correct. Try again.")
-        try:
-            user_answer = float(input("Enter an answer: "))
-        except ValueError:
-            print("Please enter a number")
-    else:
-        print("Correct")
+        while float(user_answer) != float(result):
+            print(f"{user_answer} is not correct. Try again.")
+            if time.time() - start > (game_length + 1):
+                print(round((time.time() - start),1))
+                print("Game Over")
+                return None
+            user_answer = validate_input()
+        else:
+            print("Correct")
+            run_game()
+    except TypeError:
+        return None
 
 
 def main():
-    problem = do_math()
-    result = check_answer(problem)
+    run_game()
 
 
 if __name__ == '__main__':
